738847b5a2aae8ef318a7a04bbdcd31db527b0ff
TigerSnowy <ines.f.ferreira.ds@gmail.com>
2025-04-04 12:54:26 +0200

    update 4 CI workflows

--- code/server/vite.config.ts
+++ code/server/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
 	// charger le fichier .env.test
 	dotenv.config({ path: ".env.test" });
 
+	// utiliser le serveur MySQL de GitHub Actions
+	if (process.env.GITHUB_ACTIONS) {
+		process.env.MYSQL_HOST = "127.0.0.1";
+	}
 	return {};
 });
